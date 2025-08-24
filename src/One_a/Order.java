952dcae90d2b77135558ad8e4fbb9a13407d7b30
epaulino27 <e.paulino27@ncf.edu>
2025-08-24 19:49:20 -0400

    final commit

--- src/One_a/Order.java
+++ src/One_a/Order.java
@@ -7,7 +7,7 @@ public class Order {
     MenuItem item3;
     double totalAmount;
 
-    //add order items
+    //add order items, setters
     public void addItem1(MenuItem item){
         this.item1 = item;
         return;
@@ -19,6 +19,17 @@ public class Order {
     public void addItem3(MenuItem item){
         this.item3 = item;
         return;
+    }
+
+    //getters
+    public void GetItem1(){
+        item1.displayItemInfo();
+    }
+    public void GetItem2(){
+        item2.displayItemInfo();
+    }
+    public void GetItem3(){
+        item3.displayItemInfo();
     }
 
     //calculate order total
